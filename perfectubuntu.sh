71be4b23e5dcc2f43f27ed98749966fd5d75e061
Hugo Rody <hugo.rody@unifesp.br>
2017-05-31 14:17:08 -0300

    Update perfectubuntu.sh

--- perfectubuntu.sh
+++ perfectubuntu.sh
@@ -1,2 +1,16 @@
+#Install essential tools in Ubuntu 16
+
 #GIT
 sudo apt install git
+
+#GEANY
+sudo apt install geany
+
+#UGENE
+sudo apt install ugene
+
+#R
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
+sudo apt-get update
+sudo apt-get install r-base
