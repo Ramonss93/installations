3d3b6b74068a3607a2c3a77937b3c9dbfae3190b
Hugo Rody <hugo.rody@unifesp.br>
2017-06-03 10:34:16 -0300

    Update ubuntu_essentials.sh

--- ubuntu_essentials.sh
+++ ubuntu_essentials.sh
@@ -34,6 +34,9 @@ sudo apt install ncbi-blast++
 #SAMTOOLS
 sudo apt install samtools
 
+#VCFtools
+sudo apt install vcftools
+
 #SPOTIFY
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
